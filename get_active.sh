950dc45259163991dc1f357e32d5e711b94d0afa
Ketan Bhatt <ketanbhatt1006@gmail.com>
2015-06-26 13:02:31 +0530

    Outputting active window for 10 seconds. Removed output.txt

--- get_active.sh
+++ get_active.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
-xprop -id $(xprop -root 32x '\t$0' _NET_ACTIVE_WINDOW | cut -f 2) _NET_WM_NAME WM_CLASS | cut -d" " -f3- >> output.txt
+for i in `seq 1 10`;
+        do
+                xprop -id $(xprop -root 32x '\t$0' _NET_ACTIVE_WINDOW | cut -f 2) _NET_WM_NAME WM_CLASS | cut -d" " -f3- >> output.txt
+                sleep 1
+        done
