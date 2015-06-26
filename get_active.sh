dce53dcff9c27927845fcef0886a5d444b37d973
Ketan Bhatt <ketanbhatt1006@gmail.com>
2015-06-26 13:16:32 +0530

    Running things in background now, contiuous output

--- get_active.sh
+++ get_active.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-for i in `seq 1 10`;
+echo "" > output.txt
+
+while true;
         do
                 xprop -id $(xprop -root 32x '\t$0' _NET_ACTIVE_WINDOW | cut -f 2) _NET_WM_NAME WM_CLASS | cut -d" " -f3- >> output.txt
                 sleep 1
